def720cd24358adc1b36f596452b1302afbc835d
Shlok Kaushik <shlokkaushik23@gmail.com>
2021-02-09 00:15:48 +0530

    Project 19

---
+++ script.js
@@ -0,0 +1,10 @@
+// // var search_button = getElementsByClassName('fa');
+// // var search_bar = getElementsByClassName('search')
+//
+// function appear() {
+//    document.getElementById('appear').style.display = "block";
+// }
+function appear() {
+  document.getElementById("search").style.display= "block";
+  document.getElementById("button").style.display = "none";
+}
